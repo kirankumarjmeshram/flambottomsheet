da4c3425ea9382187171dbf9d616ce9bd02565e7
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-09-16 13:37:12 +0530

    flamapp frontend assignment

--- src/PopupDiv.js
+++ src/PopupDiv.js
@@ -1,148 +1,145 @@
-// src/PopupDiv.js
+import React, { useState } from 'react';
+import './PopupDiv.css';
 
-import React, { useState } from "react";
-import "./PopupDiv.css";
+function BottomSheet() {
+  const [isDragging, setIsDragging] = useState(false);
+  const [startY, setStartY] = useState(0);
+  const [startHeight, setStartHeight] = useState(0);
+  const [sheetHeight, setSheetHeight] = useState(50); // Initial height is 50vh
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
 
-function PopupDiv() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [snapPosition, setSnapPosition] = useState("closed");
-  const [dragging, setDragging] = useState(false);
-  const [dragY, setDragY] = useState(0);
-
-  const handleButtonClick = () => {
-    setIsOpen(true);
-    setSnapPosition("half-open");
+  const updateHeight = (height) => {
+    setSheetHeight(height);
   };
 
-  const handleCloseClick = () => {
-    setIsOpen(false);
-    setSnapPosition("closed");
+  const showSheet = () => {
+    setIsSheetOpen(true);
+    updateHeight(50);
+    document.body.style.overflow = 'hidden';
   };
 
-  const handleSnap = () => {
-    if (snapPosition === "half-open") {
-      setSnapPosition("full-open");
-    } else if (snapPosition === "full-open") {
-      setSnapPosition("half-open");
+  const hideSheet = () => {
+    setIsSheetOpen(false);
+    document.body.style.overflow = 'auto';
+  };
+
+  const dragStart = (e) => {
+    setIsDragging(true);
+    const pageY = e.pageY || (e.touches && e.touches[0].pageY);
+    setStartY(pageY);
+    setStartHeight(sheetHeight);
+  };
+
+  const dragging = (e) => {
+    if (!isDragging) return;
+    const pageY = e.pageY || (e.touches && e.touches[0].pageY);
+    const delta = startY - pageY;
+    const newHeight = startHeight + (delta / window.innerHeight) * 100;
+
+    updateHeight(newHeight);
+  };
+
+  const dragStop = () => {
+    setIsDragging(false);
+
+    if (sheetHeight < 25) {
+      hideSheet();
+      updateHeight(0);
+    } else if (sheetHeight > 75) {
+      updateHeight(100);
+    } else {
+      updateHeight(50);
     }
-  };
-// Dragging Functionality
-  const handleDragStart = (e) => {
-    setDragging(true);
-    setDragY(e.clientY);
-  };
-  const handleDrag = (e) => {
-    if (dragging) {
-      const deltaY = e.clientY - dragY;
-      setDragY(e.clientY);
-
-      const popupDiv = document.querySelector(".popup-div");
-      if (popupDiv) {
-        const currentTransform = window.getComputedStyle(popupDiv).transform;
-        const currentY = currentTransform
-          ? parseInt(currentTransform.split(",")[1], 10)
-          : 0;
-        const newY = currentY + deltaY;
-        popupDiv.style.transform = `translateY(${newY}px)`;
-      }
-    }
-  };
-
-  const handleDragEnd = () => {
-    setDragging(false);
   };
 
   return (
-    <div className={`popup-container ${isOpen ? "open" : ""}`}>
-      <button onClick={handleButtonClick} className="open-button">
-        Open Popup
+    <div>
+      <button className="show-btn" onClick={showSheet}>
+        Show Bottom Sheet
       </button>
-      {isOpen && (
-        <div
-          className={`popup-div ${snapPosition}`}
-          onClick={handleSnap}
-          onTransitionEnd={handleSnap}
-
-          draggable={!dragging}
-          onDragStart={handleDragStart}
-          onDrag={handleDrag}
-          onDragEnd={handleDragEnd}
-
-        >
-          <div className="handle">Drag me</div>
-          <button className="close-button" onClick={handleCloseClick}>
-            Close
-          </button>
-          <div id="mytxt">
-          <p>
-            In the whimsical land of Flibbertigibbet, residents spoke a language
-            filled with Gobbledygook and engaged in daily activities like
-            Wobbleflopping through the town square or engaging in Quizzaciously
-            perplexing conversations. The local pastime was Lollygagging in the
-            Snickerdoodle forest while sipping Zippity-doo-dah tea and sharing
-            tales of Jibber-jabber adventures. Occasional rain showers of
-            Bumbershoots would send everyone scrambling, but they'd always end
-            up in fits of Blubbernut-induced laughter. The town's mascot, a
-            giant Skedaddling Snugglebunny named Quibblesnatch, was the source
-            of endless Fiddlesticks and Dingleberry-related amusement. Life in
-            Flibbertigibbet was indeed a Malarkey-filled, Noodlebrained, and
-            thoroughly Whatchamacallit kind of experience, where everyday
-            occurrences were a Razzleberry of Jibberwocky and Snickerdoodle
-            delights.
-          </p>
-          <p>
-            In the whimsical land of Flibbertigibbet, residents spoke a language
-            filled with Gobbledygook and engaged in daily activities like
-            Wobbleflopping through the town square or engaging in Quizzaciously
-            perplexing conversations. The local pastime was Lollygagging in the
-            Snickerdoodle forest while sipping Zippity-doo-dah tea and sharing
-            tales of Jibber-jabber adventures. Occasional rain showers of
-            Bumbershoots would send everyone scrambling, but they'd always end
-            up in fits of Blubbernut-induced laughter. The town's mascot, a
-            giant Skedaddling Snugglebunny named Quibblesnatch, was the source
-            of endless Fiddlesticks and Dingleberry-related amusement. Life in
-            Flibbertigibbet was indeed a Malarkey-filled, Noodlebrained, and
-            thoroughly Whatchamacallit kind of experience, where everyday
-            occurrences were a Razzleberry of Jibberwocky and Snickerdoodle
-            delights.
-          </p>
-          <p>
-            In the whimsical land of Flibbertigibbet, residents spoke a language
-            filled with Gobbledygook and engaged in daily activities like
-            Wobbleflopping through the town square or engaging in Quizzaciously
-            perplexing conversations. The local pastime was Lollygagging in the
-            Snickerdoodle forest while sipping Zippity-doo-dah tea and sharing
-            tales of Jibber-jabber adventures. Occasional rain showers of
-            Bumbershoots would send everyone scrambling, but they'd always end
-            up in fits of Blubbernut-induced laughter. The town's mascot, a
-            giant Skedaddling Snugglebunny named Quibblesnatch, was the source
-            of endless Fiddlesticks and Dingleberry-related amusement. Life in
-            Flibbertigibbet was indeed a Malarkey-filled, Noodlebrained, and
-            thoroughly Whatchamacallit kind of experience, where everyday
-            occurrences were a Razzleberry of Jibberwocky and Snickerdoodle
-            delights.
-          </p>
-          <p>
-            In the whimsical land of Flibbertigibbet, residents spoke a language
-            filled with Gobbledygook and engaged in daily activities like
-            Wobbleflopping through the town square or engaging in Quizzaciously
-            perplexing conversations. The local pastime was Lollygagging in the
-            Snickerdoodle forest while sipping Zippity-doo-dah tea and sharing
-            tales of Jibber-jabber adventures. Occasional rain showers of
-            Bumbershoots would send everyone scrambling, but they'd always end
-            up in fits of Blubbernut-induced laughter. The town's mascot, a
-            giant Skedaddling Snugglebunny named Quibblesnatch, was the source
-            of endless Fiddlesticks and Dingleberry-related amusement. Life in
-            Flibbertigibbet was indeed a Malarkey-filled, Noodlebrained, and
-            thoroughly Whatchamacallit kind of experience, where everyday
-            occurrences were a Razzleberry of Jibberwocky and Snickerdoodle
-            delights.
-          </p>
+      <div className={`bottom-sheet ${isSheetOpen ? 'show' : ''}`} onMouseUp={dragStop}>
+        <div className="overlay" onClick={hideSheet}></div>
+        <div className="content" style={{ height: `${sheetHeight}vh` }}>
+          <div className="header">
+            <div className="drag-icon" onMouseDown={dragStart} onMouseMove={dragging}>
+              <span></span>
+            </div>
+          </div>
+          <div className="body">
+            <h2>Bottom Sheet Content</h2>
+            <p>
+              In the whimsical land of Flibbertigibbet, residents spoke a
+              language filled with Gobbledygook and engaged in daily
+              activities like Wobbleflopping through the town square or
+              engaging in Quizzaciously perplexing conversations. The local
+              pastime was Lollygagging in the Snickerdoodle forest while
+              sipping Zippity-doo-dah tea and sharing tales of Jibber-jabber
+              adventures. Occasional rain showers of Bumbershoots would send
+              everyone scrambling, but they'd always end up in fits of
+              Blubbernut-induced laughter. The town's mascot, a giant
+              Skedaddling Snugglebunny named Quibblesnatch, was the source of
+              endless Fiddlesticks and Dingleberry-related amusement. Life in
+              Flibbertigibbet was indeed a Malarkey-filled, Noodlebrained, and
+              thoroughly Whatchamacallit kind of experience, where everyday
+              occurrences were a Razzleberry of Jibberwocky and Snickerdoodle
+              delights.
+            </p>
+            <p>
+              In the whimsical land of Flibbertigibbet, residents spoke a
+              language filled with Gobbledygook and engaged in daily
+              activities like Wobbleflopping through the town square or
+              engaging in Quizzaciously perplexing conversations. The local
+              pastime was Lollygagging in the Snickerdoodle forest while
+              sipping Zippity-doo-dah tea and sharing tales of Jibber-jabber
+              adventures. Occasional rain showers of Bumbershoots would send
+              everyone scrambling, but they'd always end up in fits of
+              Blubbernut-induced laughter. The town's mascot, a giant
+              Skedaddling Snugglebunny named Quibblesnatch, was the source of
+              endless Fiddlesticks and Dingleberry-related amusement. Life in
+              Flibbertigibbet was indeed a Malarkey-filled, Noodlebrained, and
+              thoroughly Whatchamacallit kind of experience, where everyday
+              occurrences were a Razzleberry of Jibberwocky and Snickerdoodle
+              delights.
+            </p>
+            <p>
+              In the whimsical land of Flibbertigibbet, residents spoke a
+              language filled with Gobbledygook and engaged in daily
+              activities like Wobbleflopping through the town square or
+              engaging in Quizzaciously perplexing conversations. The local
+              pastime was Lollygagging in the Snickerdoodle forest while
+              sipping Zippity-doo-dah tea and sharing tales of Jibber-jabber
+              adventures. Occasional rain showers of Bumbershoots would send
+              everyone scrambling, but they'd always end up in fits of
+              Blubbernut-induced laughter. The town's mascot, a giant
+              Skedaddling Snugglebunny named Quibblesnatch, was the source of
+              endless Fiddlesticks and Dingleberry-related amusement. Life in
+              Flibbertigibbet was indeed a Malarkey-filled, Noodlebrained, and
+              thoroughly Whatchamacallit kind of experience, where everyday
+              occurrences were a Razzleberry of Jibberwocky and Snickerdoodle
+              delights.
+            </p>
+            <p>
+              In the whimsical land of Flibbertigibbet, residents spoke a
+              language filled with Gobbledygook and engaged in daily
+              activities like Wobbleflopping through the town square or
+              engaging in Quizzaciously perplexing conversations. The local
+              pastime was Lollygagging in the Snickerdoodle forest while
+              sipping Zippity-doo-dah tea and sharing tales of Jibber-jabber
+              adventures. Occasional rain showers of Bumbershoots would send
+              everyone scrambling, but they'd always end up in fits of
+              Blubbernut-induced laughter. The town's mascot, a giant
+              Skedaddling Snugglebunny named Quibblesnatch, was the source of
+              endless Fiddlesticks and Dingleberry-related amusement. Life in
+              Flibbertigibbet was indeed a Malarkey-filled, Noodlebrained, and
+              thoroughly Whatchamacallit kind of experience, where everyday
+              occurrences were a Razzleberry of Jibberwocky and Snickerdoodle
+              delights.
+            </p>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
 
-export default PopupDiv;
+export default BottomSheet;
